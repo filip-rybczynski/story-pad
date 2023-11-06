3afb7438e5828125a0f3c35fd80f8c1a8bcae0cf
Filip Rybczyński <filipryba@gmail.com>
2023-11-06 14:55:21 +0100

    Added handling of SSO sign-in used as a sign-up

--- app/verify-user/page.tsx
+++ app/verify-user/page.tsx
@@ -4,8 +4,6 @@ import { User } from "@clerk/nextjs/api";
 import { redirect } from "next/navigation";
 
 const searchUserInDB = async () => {
-  // confirm('OK to proceed?');
-  console.log("verify user");
   const { id } = (await currentUser()) as User;
 
   const match = await prisma.user.findUnique({
@@ -13,11 +11,9 @@ const searchUserInDB = async () => {
       clerkId: id as string,
     },
   });
-  console.log("🚀 ~ file: page.tsx:14 ~ searchUserInDB ~ match:", match);
 
   if (match) redirect("/stories");
 
-  console.log("redirecting to new-user");
   if (!match) redirect("/new-user");
 };
 
