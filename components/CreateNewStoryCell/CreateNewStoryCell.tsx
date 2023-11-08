1976149ee160cca43f1b44290356638c12dd71d7
Filip Rybczyński <filipryba@gmail.com>
2023-11-08 09:28:53 +0100

    Disabled New Story button during async call

--- components/CreateNewStoryCell/CreateNewStoryCell.tsx
+++ components/CreateNewStoryCell/CreateNewStoryCell.tsx
@@ -2,19 +2,29 @@
 
 import { createNewStory } from "@/utils/api";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export const CreateNewStoryCell = () => {
   const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleClick = async () => {
+    setIsLoading(true);
     const { id: newID } = await createNewStory();
 
-    router.push(`/editor/${newID}`);
+    try {
+      router.push(`/editor/${newID}`);
+      router.refresh();
+    } catch {
+      setIsLoading(false); // If the router push doesn't work for some reason we should un-disable the button. On the other hand, it flickers, so if not needed best to avoid it
+    }
   };
 
   return (
     <button
       className="h-[200px] border-teal-600/50 hover:border-teal-600 border-4 rounded-2xl px-6 py-4 relative"
+      disabled={isLoading}
+      style={isLoading ? { opacity: "0.5" } : {}}
       onClick={handleClick}
     >
       <h2 className="text-3xl text-teal-600 absolute top-12 left-6 text-left">
